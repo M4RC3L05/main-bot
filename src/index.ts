d0a87e34e7d02018f893f3d374c3cfda801e5233
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2022-02-12 19:05:59 +0000

    Add bot entry point file
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Bot } from "#src/bot";
+import { handlers } from "#src/handlers";
+import { commands } from "#src/commands";
+
+const bot = new Bot(await handlers(), (await commands()) as any);
+await bot.init();
